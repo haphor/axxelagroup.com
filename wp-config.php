74539d301883b7921dce9c32b3b23f5c68749696
haphor <emmanuel.ola.afolabi@gmail.com>
2020-11-06 17:25:08 +0100

    updating revslider

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,22 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'axxela_new' );
+define( 'DB_NAME' , 'axxela_new' );
 
 /** MySQL database username */
-define( 'DB_USER', 'gas_new' );
+define( 'DB_USER' , 'worker' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'wX4hdYDGfmHtXXtA' );
+define( 'DB_PASSWORD' , 'En5!n3r()m' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST' , 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET' , 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define( 'DB_COLLATE' , '' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
